baa4cc17717de3a83a5ada0e59bf4bafaa5059a5
deethesaint <vtduong0912@gmail.com>
2023-04-25 21:59:24 +0700

    form closing added

--- mainForm.Designer.cs
+++ mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Blood_Bank_Management
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "mainForm";
+            this.Text = "Blood Bank Management";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- mainForm.cs
+++ mainForm.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace Blood_Bank_Management
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        public mainForm()
         {
             InitializeComponent();
+            CenterToScreen();
+            this.FormClosing += mainForm_FormClosing;
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.WindowsShutDown)
+                return;
+            if (MessageBox.Show("Are you sure you want  to quit?", "Waring", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+        }   
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
